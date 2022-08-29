2401536ebd9d09e1f84342e72bed7a5e0804848b
Nirav Panchal <97024358+Niravpanchal6268@users.noreply.github.com>
2022-08-29 10:08:54 +0530

    pending 9Q

--- module1/9/src/Q9.java
+++ module1/9/src/Q9.java
@@ -1,2 +1,10 @@
 /*Write a Java program to count the letters, spaces, numbers and other 
 characters of an input string.*/
+Public class Q9{
+ public static void main(String argu[])
+ {
+   string name= { A a java 56698}
+   
+ }
+  
+}
